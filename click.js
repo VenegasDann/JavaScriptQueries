e3b21df04b3b874f17c7fade5cadfa3c125ec683
Pom's Chicken <8dvenegas@stpatrick.org>
2017-03-23 10:55:33 -0500

    Updatesss
    Added stuffs

--- click.js
+++ click.js
@@ -1,8 +1,27 @@
 //declare samecolor() here
-function samecolor(hex){
+function samecolor(){
+    const doc = document;
+    let color = "#009900";
 
+    let the_div = doc.querySelectorAll("#samecolor div");
+
+    divs[0].style.backgroundColor = color;
+    divs[1].style.opacity = 0.75;
+    divs[2].style.height= "250px"
 }
+
+
+
+    for(let i = 0; i <the_div.length; i++)
+    {
+        the_div.style.color = '#f00';
+    }
+}
+
+
+
 //declare diffcolor() here
-function diffcolor(hex){
+function diffcolor(){
+    let color = "#9933s00"
 
 }
--- resize.js
+++ resize.js
@@ -1,4 +1,17 @@
 //Declare tabletSize() here
+function samecolor(){
+    const doc = document;
+    let the_div = doc.querySelectorAll(#samecolor > div); //array
+    for(let i = 0; i <the_div.length; i++)
+    {
+        the_div.style.color = '#f00';
+    }
+}
 
 
 //Declare phoneSize() here
+
+fucntion phoneSize(){
+    const doc = document;
+
+}
